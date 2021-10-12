4ec76888299c8eb80622dfe13a8adcc61a1d76c4
SifatAsif <sifatasif101@gmail.com>
2021-10-13 03:59:15 +0600

    Initial commit

---
+++ 1cel_to_fahr.py
@@ -0,0 +1,7 @@
+def cel_to_fahr(c):
+    f = (float(c)*(9/5))+32
+    return f
+#comment
+"""multiline comment"""
+c = input("Enter cel:")
+print(cel_to_fahr(c))
---
+++ 2strn_length.py
@@ -0,0 +1,4 @@
+def str_length(star):
+    return len(star)
+star=str(input(""))
+print("string length=",str_length(star))
